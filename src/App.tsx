190775fd6da6b2800ed4107baa99cf38b1cd751a
Humberto Yusta Gómez <humbertoyusta02@gmail.com>
2023-03-01 00:52:24 +0100

    feat/add-body-wrapper-component

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import SearchBar from "./components/SearchBar";
+import useSWR from "swr";
+import axios from "axios";
+import BodyWrapper from "./components/BodyWrapper";
+import Header from "./components/Header";
+
+const fetcher = (url: string) => axios.get(url);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [search, setSearch] = useState("");
+
+    /* fetch data using useSWR from https://youtube.thorsteinsson.is/api/search?q={search}*/
+    // const {data, error, isLoading} = useSWR(`https://youtube.thorsteinsson.is/api/search?q=${search}`, fetcher);
+    //
+    // if (!error && !isLoading && data)
+    //     console.log(data.data);
+
+    return (
+        <>
+            <Header>
+                <SearchBar handleSearchSubmit={(searchString) => setSearch(searchString)} />
+            </Header>
+            <BodyWrapper>
+                {/*{!error && !isLoading && data && data.data.map((video: any) => (*/}
+                {/*    <div key={video.id}>*/}
+                {/*        <h1>{video.title}</h1>*/}
+                {/*        <img src={video.thumbnail} alt={video.title} />*/}
+                {/*    </div>*/}
+                {/*))}*/}
+            </BodyWrapper>
+        </>
+    );
 }
 
 export default App;

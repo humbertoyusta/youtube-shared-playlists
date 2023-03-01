5a7dd449b8695ef2426aea1a47007bddd50f7b79
Humberto Yusta Gómez <humbertoyusta02@gmail.com>
2023-03-01 23:40:47 +0100

    feat/add-get-video-list-hook

---
+++ src/hooks/useGetVideoList.ts
@@ -0,0 +1,32 @@
+import axios from "axios";
+import useSWR from "swr";
+import VideoInterface from "../interfaces/VideoInterface";
+
+const fetcher = (url: string) => axios.get(url);
+
+type useGetVideoListReturnType = {
+    videoList: VideoInterface[],
+    error: any,
+    isLoading: boolean,
+}
+
+export default function useGetVideoList(search: string): useGetVideoListReturnType {
+    const { data, error, isLoading } = useSWR(
+        `https://youtube.thorsteinsson.is/api/search?q=${search}`,
+        fetcher,
+    );
+
+    if (error)
+        return {videoList: [], error, isLoading};
+
+    if (isLoading)
+        return {videoList: [], error, isLoading};
+
+    const videoList = data?.data?.map((video: any) => ({
+        id: video.id.videoId,
+        title: video.snippet.title,
+        thumbnail: video.snippet?.thumbnails?.url,
+    }));
+
+    return {videoList, error, isLoading};
+}

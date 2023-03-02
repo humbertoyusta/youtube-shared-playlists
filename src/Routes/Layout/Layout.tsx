f7d7a752469c654bae1e1fd34de0eeb9411a061a
Humberto Yusta Gómez <humbertoyusta02@gmail.com>
2023-03-03 00:50:43 +0100

    feat/add-layout-route

---
+++ src/Routes/Layout/Layout.tsx
@@ -0,0 +1,19 @@
+import Header from "../../Components/Header";
+import BodyWrapper from "../../Components/BodyWrapper";
+import {Outlet, useLocation} from "react-router-dom";
+import React from "react";
+
+export default function Layout() {
+    const location = useLocation();
+    let withHeader = (location.pathname !== "/");
+    return (
+        <>
+            {withHeader &&
+                <Header />
+            }
+            <BodyWrapper>
+                <Outlet />
+            </BodyWrapper>
+        </>
+    );
+};

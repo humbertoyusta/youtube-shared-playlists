360d806e19e88c3d6b21cceebff7d809f40aeff3
Humberto Yusta Gómez <humbertoyusta02@gmail.com>
2023-03-03 00:50:26 +0100

    fix/minor-styling-fix

--- src/Components/BodyWrapper/BodyWrapper.styled.ts
+++ src/Components/BodyWrapper/BodyWrapper.styled.ts
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const BodyWrapper = styled.div`
     display: flex;
     flex-direction: row;
-    flex-wrap: nowrap;
+    flex-wrap: wrap;
     align-items: start;
     align-content: start;
     width: 96vw;
--- src/Components/Header/Header.styled.ts
+++ src/Components/Header/Header.styled.ts
@@ -6,7 +6,7 @@ export const HeaderStyled = styled.div`
     display: flex;
     flex-direction: row;
     flex-wrap: nowrap;
-    justify-content: center;
+    justify-content: space-between;
     align-items: center;
     width: 100vw;
 `;

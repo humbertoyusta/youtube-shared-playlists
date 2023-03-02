44ded076cd3c96e8726825911bc9448f63f458d1
Humberto Yusta Gómez <humbertoyusta02@gmail.com>
2023-03-03 00:50:52 +0100

    feat/add-welcome-route

---
+++ src/Routes/Welcome/Welcome.styled.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const WelcomeWrapperStyled = styled.div`
+    display: flex;
+    flex-direction: column;
+    flex-wrap: nowrap;
+    justify-content: space-evenly;
+    align-items: center;
+    width: 100vw;
+    height: 60vh;
+`;
---
+++ src/Routes/Welcome/Welcome.tsx
@@ -0,0 +1,12 @@
+import SearchBar from "../../Components/SearchBar";
+import {WelcomeWrapperStyled} from "./Welcome.styled";
+import Logo from "../../Components/Logo";
+
+export default function Welcome() {
+    return (
+        <WelcomeWrapperStyled>
+            <Logo src={"/logo.png"} alt={"Logo"} link={"/"} big />
+            <SearchBar />
+        </WelcomeWrapperStyled>
+    )
+}

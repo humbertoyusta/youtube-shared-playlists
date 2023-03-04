4a65bccdd7577af5b064442488217b717f913f54
Humberto Yusta Gómez <humbertoyusta02@gmail.com>
2023-03-04 13:16:12 +0100

    feat/add-copy-link-button-component

---
+++ src/Components/CopyLinkButton/CopyLinkButton.tsx
@@ -0,0 +1,20 @@
+import {CopyLinkButtonStyled, CopyLinkTextStyled} from "./CopyLinkButton.styled";
+import {useState} from "react";
+import {ReactComponent as CopyLinkIcon} from "./copy.svg";
+
+export default function CopyLinkButton ({link}: {link: string}) {
+    const [isCopied, setIsCopied] = useState(false);
+
+    function handleCopy() {
+        navigator.clipboard.writeText(link);
+        setIsCopied(true);
+        setTimeout(() => setIsCopied(false), 2000);
+    }
+
+    return (
+        <CopyLinkButtonStyled onClick={handleCopy}>
+            <CopyLinkIcon />
+            <CopyLinkTextStyled>{isCopied ? "Copied!" : "Copy Link"}</CopyLinkTextStyled>
+        </CopyLinkButtonStyled>
+    );
+}

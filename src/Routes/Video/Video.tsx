0cbed27a0926e8bdee3e48ed54a94eacb6b7c545
Humberto Yusta Gómez <humbertoyusta02@gmail.com>
2023-03-04 10:57:54 +0100

    feat/scroll-to-top-on-link-click

--- src/Routes/Video/Video.tsx
+++ src/Routes/Video/Video.tsx
@@ -2,17 +2,22 @@ import VideoPlayer from "../../Components/VideoPlayer";
 import {useParams} from "react-router-dom";
 import VideoList from "../../Components/VideoList";
 import {VideoWrapperStyled} from "./Video.styled";
+import ScrollToTop from "../../Components/ScrollToTop";
+import React from "react";
 
 export default function Video() {
     // get video id from url params
     const {videoId} = useParams<{videoId: string}>();
 
     return (
-        <VideoWrapperStyled>
-            {videoId &&
-                <VideoPlayer videoId={videoId} />
-            }
-            <VideoList fullWidth={false} />
-        </VideoWrapperStyled>
+        <>
+            <ScrollToTop />
+            <VideoWrapperStyled>
+                {videoId &&
+                    <VideoPlayer videoId={videoId} />
+                }
+                <VideoList fullWidth={false} />
+            </VideoWrapperStyled>
+        </>
     );
 }

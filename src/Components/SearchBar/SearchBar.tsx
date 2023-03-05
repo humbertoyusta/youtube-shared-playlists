1f7357f3ee810dfb597ad49a5353ef6f15b112c7
Humberto Yusta Gómez <humbertoyusta02@gmail.com>
2023-03-05 16:52:54 +0100

    feat/add-animation-to-search-bar-and-welcome-page

--- src/Components/SearchBar/SearchBar.tsx
+++ src/Components/SearchBar/SearchBar.tsx
@@ -1,8 +1,9 @@
 import {useForm} from "react-hook-form";
 import {useLocation, useNavigate} from "react-router-dom";
 import {SearchBarFormStyled, SearchBarInputStyled, SearchBarSubmitStyled} from "./SearchBar.styled";
+import {motion} from "framer-motion";
 
-export default function SearchBar() {
+export default function SearchBar({delay = 0}: {delay?: number}) {
     const {register, handleSubmit} = useForm();
     const navigate = useNavigate();
     const location = useLocation();
@@ -17,16 +18,28 @@ export default function SearchBar() {
     }
 
     return (
-        <SearchBarFormStyled
-            onSubmit={handleSubmit((event) => handleSearchSubmit(event.search))}
+        <motion.div
+            initial={{ opacity: 0, x: 60 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{
+                type: "easeInOut",
+                stiffness: 260,
+                damping: 20,
+                delay: delay,
+                duration: 0.4,
+            }}
         >
-            <SearchBarInputStyled
-                type="text"
-                placeholder="Search"
-                defaultValue={initialSearch}
-                {...register("search")}
-            />
-            <SearchBarSubmitStyled type="submit" />
-        </SearchBarFormStyled>
+            <SearchBarFormStyled
+                onSubmit={handleSubmit((event) => handleSearchSubmit(event.search))}
+            >
+                <SearchBarInputStyled
+                    type="text"
+                    placeholder="Search"
+                    defaultValue={initialSearch}
+                    {...register("search")}
+                />
+                <SearchBarSubmitStyled type="submit" />
+            </SearchBarFormStyled>
+        </motion.div>
     );
 }
--- src/Routes/Welcome/Welcome.tsx
+++ src/Routes/Welcome/Welcome.tsx
@@ -1,12 +1,38 @@
 import SearchBar from "../../Components/SearchBar";
 import {WelcomeWrapperStyled} from "./Welcome.styled";
 import Logo from "../../Components/Logo";
+import {motion, AnimatePresence} from "framer-motion";
+
+const logoVariants = {
+    hidden: {
+        opacity: 0,
+        rotate: 0,
+        scale: 0,
+    },
+    visible: {
+        opacity: 1,
+        rotate: 360,
+        scale: 1,
+        transition: {
+            duration: 1.2,
+            ease: "easeInOut",
+        },
+    },
+};
 
 export default function Welcome() {
     return (
         <WelcomeWrapperStyled>
-            <Logo src={"/logo.png"} alt={"Logo"} link={"/"} big />
-            <SearchBar />
+            <AnimatePresence>
+                <motion.div
+                    variants={logoVariants}
+                    initial="hidden"
+                    animate="visible"
+                >
+                    <Logo src={"/logo.png"} alt={"Logo"} link={"/"} big />
+                </motion.div>
+                <SearchBar delay={1.2} />
+            </AnimatePresence>
         </WelcomeWrapperStyled>
     )
 }

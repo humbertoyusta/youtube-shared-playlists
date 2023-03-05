edb6843c069467bd03f5763bcf1a10e470db73ff
Humberto Yusta Gómez <humbertoyusta02@gmail.com>
2023-03-05 17:57:13 +0100

    fix/remove-unused-animate-presence

--- src/Routes/Welcome/Welcome.tsx
+++ src/Routes/Welcome/Welcome.tsx
@@ -1,15 +1,12 @@
 import SearchBar from "../../Components/SearchBar";
 import {WelcomeWrapperStyled} from "./Welcome.styled";
 import Logo from "../../Components/Logo";
-import {AnimatePresence} from "framer-motion";
 
 export default function Welcome() {
     return (
         <WelcomeWrapperStyled>
-            <AnimatePresence>
-                <Logo src={"/logo.png"} alt={"Logo"} link={"/"} big animated />
-                <SearchBar delay={1.2} />
-            </AnimatePresence>
+            <Logo src={"/logo.png"} alt={"Logo"} link={"/"} big animated />
+            <SearchBar delay={1.2} />
         </WelcomeWrapperStyled>
     )
 }

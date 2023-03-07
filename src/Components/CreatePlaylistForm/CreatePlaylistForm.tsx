8354be043eda4d4d182ef34f832fe85a9387cb04
Humberto Yusta Gómez <humbertoyusta02@gmail.com>
2023-03-07 23:45:33 +0100

    feat/add-create-playlist-form

---
+++ src/Components/CreatePlaylistForm/CreatePlaylistForm.tsx
@@ -0,0 +1,47 @@
+import Input from "../Input";
+import {FieldValues, useForm} from "react-hook-form";
+import {CreatePlaylistFormStyled} from "./CreatePlaylistForm.styled";
+import Button from "../Buttons/Button";
+import useCreatePlaylist from "../../Hooks/PlaylistApiHooks/useCreatePlaylist";
+import ErrorAnimation from "../Animations/ErrorAnimation";
+import LoadingAnimation from "../Animations/LoadingAnimation";
+import {useNavigate} from "react-router-dom";
+import {useEffect} from "react";
+
+export default function CreatePlaylistForm() {
+    const {handleSubmit, formState, register} = useForm();
+    const {mutate, data, isLoading, isError} = useCreatePlaylist();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (data)
+            navigate(`/playlists/${data.data.id}/edit`);
+    }, [data, navigate]);
+
+    async function handleCreatePlaylist({name}: FieldValues) {
+        mutate(name);
+    }
+
+    if (isLoading)
+        return <LoadingAnimation/>
+
+    if (isError)
+        return <ErrorAnimation/>
+
+    return (
+        <CreatePlaylistFormStyled onSubmit={handleSubmit(handleCreatePlaylist)}>
+            <Input
+                errors={formState.errors.name?.message?.toString()}
+                label={"Name"}
+                {...register("name", {required: "Name is required"})}
+            />
+            <Button
+                text={"Create Playlist"}
+                type="submit"
+                disabled={false}
+            />
+        </CreatePlaylistFormStyled>
+    )
+}
+
+
--- src/Routes/Welcome/Welcome.tsx
+++ src/Routes/Welcome/Welcome.tsx
@@ -1,12 +1,29 @@
 import SearchBar from "../../Components/SearchBar";
 import {WelcomeWrapperStyled} from "./Welcome.styled";
 import Logo from "../../Components/Logo";
+import Button from "../../Components/Buttons/Button";
+import {useState} from "react";
+import Modal from "../../Components/Modal";
+import CreatePlaylistForm from "../../Components/CreatePlaylistForm";
 
 export default function Welcome() {
+    const [showForm, setShowForm] = useState(false);
+
     return (
         <WelcomeWrapperStyled>
-            <Logo src={"/logo.png"} alt={"Logo"} link={"/"} big animated />
-            <SearchBar delay={1.2} />
+            {showForm &&
+                <Modal onClose={() => setShowForm(false)}>
+                    <CreatePlaylistForm/>
+                </Modal>
+            }
+            <Logo src={"/logo.png"} alt={"Logo"} link={"/"} big animated/>
+            <SearchBar delay={1.2}/>
+            <Button
+                text={"Create Playlist"}
+                animated
+                delay={1.5}
+                onClick={() => setShowForm(true)}
+            />
         </WelcomeWrapperStyled>
     )
 }

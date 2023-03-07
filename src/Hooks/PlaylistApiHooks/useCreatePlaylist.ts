a9c84f67e9e9d1993b28744aaf4bd22cefae24cd
Humberto Yusta Gómez <humbertoyusta02@gmail.com>
2023-03-07 23:43:12 +0100

    fix/pass-name-to-create-playlist

--- src/Hooks/PlaylistApiHooks/useCreatePlaylist.ts
+++ src/Hooks/PlaylistApiHooks/useCreatePlaylist.ts
@@ -4,9 +4,9 @@ import axios from "axios";
 const baseUrl = 'https://youtube.thorsteinsson.is';
 
 export default function useCreatePlaylist() {
-    return useMutation(() =>
+    return useMutation((name: string) =>
         axios.post(baseUrl + '/api/playlists', {
-            name: Math.random().toString(),
+            name: name,
         }),
     );
 }

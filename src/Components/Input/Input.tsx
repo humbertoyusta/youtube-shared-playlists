18b6b795da55694067f66e40e06fbdcb392b86d2
Humberto Yusta Gómez <humbertoyusta02@gmail.com>
2023-03-07 23:44:47 +0100

    feat/add-input-component

---
+++ src/Components/Input/Input.tsx
@@ -0,0 +1,28 @@
+import {forwardRef} from "react";
+import {ErrorTextStyled, InputStyled, LabelStyled} from "./Input.styled";
+
+type InputProps = {
+    name: string;
+    label: string;
+    errors?: string;
+    rest?: any;
+}
+
+const Input = forwardRef<HTMLInputElement, InputProps>(
+    ({name, label, errors, ...rest}, ref) => {
+        return (
+            <div>
+                <LabelStyled htmlFor={name}>{label}</LabelStyled>
+                <InputStyled
+                    ref={ref}
+                    name={name}
+                    hasError={Boolean(errors)}
+                    {...rest}
+                />
+                {errors && <ErrorTextStyled>{errors}</ErrorTextStyled>}
+            </div>
+        );
+    }
+);
+
+export default Input;

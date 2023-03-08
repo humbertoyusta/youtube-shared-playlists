e751cf93b34228a7af713760072b8e25be0433f6
Humberto Yusta Gómez <humbertoyusta02@gmail.com>
2023-03-08 22:13:26 +0100

    refactor/unify-button-components

--- src/Components/Buttons/Button/Button.tsx
+++ src/Components/Buttons/Button/Button.tsx
@@ -1,23 +1,34 @@
-import {ButtonMotionAnimation, ButtonStyled, ButtonTextStyled,} from "./Button.styled";
 import React from "react";
+import {ReactComponent as ShuffleIcon} from "./Icons/shuffle.svg";
+import {ReactComponent as PlayIcon} from "./Icons/play.svg";
+import {ReactComponent as RemoveIcon} from "./Icons/remove.svg";
+import {ReactComponent as AddIcon} from "./Icons/add.svg";
+import {ButtonMotionAnimation, ButtonStyled, ButtonTextStyled} from "./Button.styled";
 
 type ButtonProps = {
     text: string;
+    name?: string;
     animated?: boolean;
     delay?: number;
 }
 
-export default function Button({text, animated = false, delay = 0, ...rest}:
+export default function Button({name, text, animated = false, delay = 0, ...rest}:
                                    ButtonProps & React.ButtonHTMLAttributes<HTMLButtonElement>) {
-    return animated ? (
-        <ButtonMotionAnimation delay={delay}>
-            <ButtonStyled {...rest}>
-                <ButtonTextStyled>{text}</ButtonTextStyled>
-            </ButtonStyled>
-        </ButtonMotionAnimation>
-    ) : (
+    const button = (
         <ButtonStyled {...rest}>
-            <ButtonTextStyled>{text}</ButtonTextStyled>
+            {name === "shuffle" && <ShuffleIcon/>}
+            {name === "play" && <PlayIcon/>}
+            {name === "remove" && <RemoveIcon/>}
+            {name === "add" && <AddIcon/>}
+            {text &&
+                <ButtonTextStyled>{text}</ButtonTextStyled>
+            }
         </ButtonStyled>
     );
+
+    return animated ? (
+        <ButtonMotionAnimation delay={delay}>
+            {button}
+        </ButtonMotionAnimation>
+    ) : button;
 }

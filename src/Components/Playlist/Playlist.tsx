b46a0c726b544863d3d1ffd4baade48eccf0cae4
Humberto Yusta Gómez <humbertoyusta02@gmail.com>
2023-03-08 11:15:17 +0100

    feat/add-button-list-div

--- src/Components/Playlist/Playlist.tsx
+++ src/Components/Playlist/Playlist.tsx
@@ -1,14 +1,22 @@
 import VideoInterface from "../../Interfaces/VideoInterface";
 import VideoItem from "../VideoItem";
-import {PlaylistStyled} from "./Playlist.styled";
+import {PlaylistButtonListStyled, PlaylistStyled} from "./Playlist.styled";
 import AddSomethingAnimation from "../Animations/AddSomethingAnimation";
+import CopyLinkButton from "../Buttons/CopyLinkButton";
+import {useLocation, useNavigate, useParams} from "react-router-dom";
+import ButtonWithIcon from "../Buttons/ButtonWithIcon";
 
 type PlaylistProps = {
     videos: VideoInterface[];
     removeVideoFromPlaylist?: (video: VideoInterface) => void;
+    shuffleVideos?: () => void;
 }
 
-export default function Playlist({videos, removeVideoFromPlaylist}: PlaylistProps) {
+export default function Playlist({videos, removeVideoFromPlaylist, shuffleVideos}: PlaylistProps) {
+    const location = useLocation();
+    const navigate = useNavigate();
+    const {playlistId} = useParams<{ playlistId: string }>();
+
     if (!videos.length)
         return (
             <PlaylistStyled>
@@ -18,6 +26,13 @@ export default function Playlist({videos, removeVideoFromPlaylist}: PlaylistProp
 
     return (
         <PlaylistStyled>
+            <PlaylistButtonListStyled>
+                <ButtonWithIcon key="play" text="Play" onClick={() => navigate(`/playlists/${playlistId}/play`)}/>
+                {shuffleVideos && videos.length >= 2 &&
+                    <ButtonWithIcon key="shuffle" text="Shuffle" onClick={() => shuffleVideos()}/>
+                }
+                <CopyLinkButton link={window.location.origin + location.pathname}/>
+            </PlaylistButtonListStyled>
             {videos.map(video => (
                 <VideoItem
                     key={video.id}

9d6e2d3a3e308f220294f221a131a6917a5cc76d
Humberto Yusta Gómez <humbertoyusta02@gmail.com>
2023-03-09 09:39:15 +0100

    feat/add-config-file

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,8 @@
 export const YoutubeApiConfig = {
     baseUrl: process.env.REACT_APP_YOUTUBE_API_BASE_URL,
 };
+
+export const RealYoutubeApiConfig = {
+    baseUrl: process.env.REACT_APP_REAL_YOUTUBE_API_BASE_URL,
+    apiKey: process.env.REACT_APP_REAL_YOUTUBE_API_KEY,
+};

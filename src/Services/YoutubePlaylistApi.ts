a247e10d303f7b448c180260faca1782c99d5336
Humberto Yusta Gómez <humbertoyusta02@gmail.com>
2023-03-09 10:58:30 +0100

    feat/add-service-for-api-requests

---
+++ src/Services/YoutubePlaylistApi.ts
@@ -0,0 +1,23 @@
+import axios from "axios";
+import { YoutubeApiConfig } from "../config";
+import VideoInterface from "../Interfaces/VideoInterface";
+import { encodeVideo } from "../Utils/parseVideo";
+
+export const createPlaylist = async (name: string) => {
+    const playlist = await axios.post(
+        YoutubeApiConfig.baseUrl + "/api/playlists",
+        { name: name }
+    );
+    return playlist.data.id;
+};
+
+export const updatePlaylist = async (
+    playlistId: string,
+    name: string,
+    videos: VideoInterface[]
+) => {
+    await axios.put(YoutubeApiConfig.baseUrl + `/api/playlists/${playlistId}`, {
+        name: name,
+        videos: videos.map((video) => encodeVideo(video)),
+    });
+};

6a847437ff918896b3297dc46c0116d42ba70aff
Humberto Yusta Gómez <humbertoyusta02@gmail.com>
2023-03-09 11:03:16 +0100

    feat/poll-changes-every-2-seconds

--- src/Hooks/PlaylistApiHooks/useGetPlaylist.ts
+++ src/Hooks/PlaylistApiHooks/useGetPlaylist.ts
@@ -3,10 +3,16 @@ import axios from "axios";
 import { YoutubeApiConfig } from "../../config";
 
 export default function useGetPlaylist(playlistId: string) {
-    return useQuery(["playlist", playlistId], async () => {
-        const { data } = await axios.get(
-            YoutubeApiConfig.baseUrl + `/api/playlists/${playlistId}`
-        );
-        return data;
-    });
+    return useQuery(
+        ["playlist", playlistId],
+        async () => {
+            const { data } = await axios.get(
+                YoutubeApiConfig.baseUrl + `/api/playlists/${playlistId}`
+            );
+            return data;
+        },
+        {
+            refetchInterval: 2000,
+        }
+    );
 }

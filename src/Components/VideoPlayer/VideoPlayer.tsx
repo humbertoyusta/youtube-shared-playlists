63bfbd8544888a5a3f416492ae3a21f6755180a3
Humberto Yusta Gómez <humbertoyusta02@gmail.com>
2023-03-09 21:40:27 +0100

    feat/sync-playback-with-web-sockets

--- src/Components/VideoPlayer/VideoPlayer.tsx
+++ src/Components/VideoPlayer/VideoPlayer.tsx
@@ -9,11 +9,16 @@ import LoadingAnimation from "../Animations/LoadingAnimation";
 import ErrorAnimation from "../Animations/ErrorAnimation";
 import CopyLinkButton from "../Buttons/CopyLinkButton";
 import { useLocation } from "react-router-dom";
+import { useEffect, useRef } from "react";
+import { io } from "socket.io-client";
+import ReactPlayer from "react-player";
 
 type VideoPlayerProps = {
     videoId: string;
     playNextVideo?: () => void;
 };
+
+const socket = io("http://localhost:3001");
 
 export default function VideoPlayer({
     videoId,
@@ -21,6 +26,38 @@ export default function VideoPlayer({
 }: VideoPlayerProps) {
     const { video, isLoading, error } = useGetVideo(videoId);
     const location = useLocation();
+    const videoRef = useRef<ReactPlayer>(null);
+
+    const handleRemotePlay = (seconds: number) => {
+        videoRef.current?.getInternalPlayer()?.playVideo();
+        const currentTime = videoRef.current?.getCurrentTime();
+        if (currentTime) {
+            const currentTimeDiff = Math.abs(currentTime - seconds);
+            if (currentTimeDiff > 0.5) {
+                videoRef.current?.getInternalPlayer()?.seekTo(seconds);
+            }
+        }
+    };
+    const handleRemotePause = (seconds: number) => {
+        videoRef.current?.getInternalPlayer()?.pauseVideo();
+        const currentTime = videoRef.current?.getCurrentTime();
+        if (currentTime) {
+            const currentTimeDiff = Math.abs(currentTime - seconds);
+            if (currentTimeDiff > 0.5) {
+                videoRef.current?.getInternalPlayer()?.seekTo(seconds);
+            }
+        }
+    };
+
+    useEffect(() => {
+        socket.on("play", handleRemotePlay);
+        socket.on("pause", handleRemotePause);
+
+        return () => {
+            socket.off("play", handleRemotePlay);
+            socket.off("pause", handleRemotePause);
+        };
+    }, []);
 
     if (isLoading)
         return (
@@ -42,9 +79,16 @@ export default function VideoPlayer({
             <VideoPlayerWrapperStyled>
                 <VideoPlayerIframeStyled
                     url={`https://www.youtube.com/embed/${video.id}`}
-                    playing={true}
-                    controls={true}
+                    playing
+                    controls
                     onEnded={playNextVideo ? () => playNextVideo() : () => {}}
+                    onPlay={() =>
+                        socket.emit("play", videoRef.current?.getCurrentTime())
+                    }
+                    onPause={() =>
+                        socket.emit("pause", videoRef.current?.getCurrentTime())
+                    }
+                    ref={videoRef}
                 />
                 <VideoPlayerTitleStyled>{video.title}</VideoPlayerTitleStyled>
                 <VideoPlayerDivStyled>
